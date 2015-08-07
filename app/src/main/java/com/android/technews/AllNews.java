c16d4b6c0789deff2362a7d0cdd976ac0db29f97
Sahil Mutneja <sahil.mutneja26@gmail.com>
2015-08-07 16:02:36 +0530

    Added for all news

--- app/src/main/java/com/android/technews/AllNews.java
+++ app/src/main/java/com/android/technews/AllNews.java
@@ -5,18 +5,52 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.technews.adapters.NewsAdapter;
+import com.android.technews.models.Collection1;
+import com.android.technews.models.TechNewsResponse;
+import com.android.technews.network.NewsAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by smutneja03 on 07/08/15.
  */
 public class AllNews extends Fragment {
 
+    private ListView listView;
+    private List<Collection1> newsList = new ArrayList<>();
+    private NewsAdapter newsAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.all_news, container, false);
+        View view = inflater.inflate(R.layout.all_news, container, false);
+        listView = (ListView) view.findViewById(R.id.all_news);
 
-        return rootView;
+        NewsAPI.getApi().getMusicList(new Callback<TechNewsResponse>() {
+
+            @Override
+            public void success(TechNewsResponse musicApiResponse, Response response) {
+                newsAdapter = new NewsAdapter(getActivity(), musicApiResponse.getResults().getCollection1());
+                newsList = musicApiResponse.getResults().getCollection1();
+                listView.setAdapter(newsAdapter);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+
+        });
+
+        return view;
     }
 }
